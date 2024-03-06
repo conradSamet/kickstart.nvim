5b4225d004304f93ddeac42c45165d77e7434e90
Samet Conrad <contact.sametconrad@gmail.com>
2024-03-06 21:42:06 +0100

    added some lsp

--- init.lua
+++ init.lua
@@ -548,6 +548,7 @@ require('lazy').setup {
         phpactor = {},
         intelephense = {},
         tailwindcss = {},
+        prismals = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
